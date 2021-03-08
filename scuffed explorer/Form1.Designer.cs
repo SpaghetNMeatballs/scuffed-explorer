c0557cb813b7a7b9e36d96f397f2883c1974bbd7
ghost <ghostsmans@gmail.com>
2021-03-08 20:32:16 +0300

    Restored Button[] from ListView

--- scuffed explorer/Form1.Designer.cs
+++ scuffed explorer/Form1.Designer.cs
@@ -29,6 +29,7 @@ namespace scuffed_explorer
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
@@ -36,6 +37,12 @@ namespace scuffed_explorer
             this.button3 = new System.Windows.Forms.Button();
             this.treeView1 = new System.Windows.Forms.TreeView();
             this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.moveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.renameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
@@ -47,11 +54,12 @@ namespace scuffed_explorer
             this.button1.Location = new System.Drawing.Point(0, 0);
             this.button1.Margin = new System.Windows.Forms.Padding(0);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(30, 30);
+            this.button1.Size = new System.Drawing.Size(40, 37);
             this.button1.TabIndex = 0;
             this.button1.Tag = "Back";
             this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.moveUp);
             // 
             // button2
             // 
@@ -59,10 +67,10 @@ namespace scuffed_explorer
             this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button2.FlatAppearance.BorderSize = 0;
-            this.button2.Location = new System.Drawing.Point(30, 0);
+            this.button2.Location = new System.Drawing.Point(40, 0);
             this.button2.Margin = new System.Windows.Forms.Padding(0);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(30, 30);
+            this.button2.Size = new System.Drawing.Size(40, 37);
             this.button2.TabIndex = 1;
             this.button2.Tag = "Refresh";
             this.button2.UseVisualStyleBackColor = true;
@@ -73,20 +81,20 @@ namespace scuffed_explorer
             this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox1.Location = new System.Drawing.Point(90, 0);
+            this.textBox1.Location = new System.Drawing.Point(120, 0);
             this.textBox1.Margin = new System.Windows.Forms.Padding(0);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(711, 30);
+            this.textBox1.Size = new System.Drawing.Size(947, 36);
             this.textBox1.TabIndex = 2;
             // 
             // button3
             // 
             this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
-            this.button3.Location = new System.Drawing.Point(801, 0);
+            this.button3.Location = new System.Drawing.Point(1068, 0);
             this.button3.Margin = new System.Windows.Forms.Padding(0);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(30, 30);
+            this.button3.Size = new System.Drawing.Size(40, 37);
             this.button3.TabIndex = 4;
             this.button3.Tag = "Go";
             this.button3.UseVisualStyleBackColor = true;
@@ -96,39 +104,82 @@ namespace scuffed_explorer
             // 
             this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
-            this.treeView1.Location = new System.Drawing.Point(0, 30);
+            this.treeView1.Location = new System.Drawing.Point(0, 37);
             this.treeView1.Margin = new System.Windows.Forms.Padding(0);
             this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(159, 541);
+            this.treeView1.Size = new System.Drawing.Size(211, 665);
             this.treeView1.TabIndex = 5;
             // 
             // button4
             // 
             this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
-            this.button4.Location = new System.Drawing.Point(60, 0);
+            this.button4.Location = new System.Drawing.Point(80, 0);
             this.button4.Margin = new System.Windows.Forms.Padding(0);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(30, 30);
+            this.button4.Size = new System.Drawing.Size(40, 37);
             this.button4.TabIndex = 6;
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.moveUp);
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Location = new System.Drawing.Point(215, 40);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(903, 602);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.moveToolStripMenuItem,
+            this.renameToolStripMenuItem,
+            this.deleteToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(211, 104);
+            // 
+            // moveToolStripMenuItem
+            // 
+            this.moveToolStripMenuItem.Name = "moveToolStripMenuItem";
+            this.moveToolStripMenuItem.Size = new System.Drawing.Size(210, 24);
+            this.moveToolStripMenuItem.Text = "Move";
+            // 
+            // renameToolStripMenuItem
+            // 
+            this.renameToolStripMenuItem.Name = "renameToolStripMenuItem";
+            this.renameToolStripMenuItem.Size = new System.Drawing.Size(210, 24);
+            this.renameToolStripMenuItem.Text = "Rename";
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(210, 24);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(842, 573);
+            this.ClientSize = new System.Drawing.Size(1123, 705);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.treeView1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.MinimumSize = new System.Drawing.Size(500, 300);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MinimumSize = new System.Drawing.Size(661, 358);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -142,6 +193,11 @@ namespace scuffed_explorer
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.TreeView treeView1;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem moveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem renameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
     }
 }
 
--- scuffed explorer/Form1.cs
+++ scuffed explorer/Form1.cs
@@ -13,8 +13,7 @@ namespace scuffed_explorer
 {
     public partial class Form1 : Form
     {
-
-        private PictureBox[] directoryButtons;
+        private Button[] directoryButtons;
         private Label[] directoryLabels;
         TableLayoutPanel tableLayoutPanel1 = new TableLayoutPanel();
         public Form1()
@@ -22,11 +21,12 @@ namespace scuffed_explorer
             InitializeComponent();
         }
         private void drawDirectory(String path) {
+            groupBox1.Controls.Clear();
             textBox1.Text = (path);
             String[] childDirs = Directory.GetDirectories(path);
             String[] childFiles = Directory.GetFiles(path);
             int total = childDirs.Length + childFiles.Length;
-            int xoffset = 1, yoffset = 1, buttonsize = 60;
+            int xoffset = 20, yoffset = 30, buttonsize = 60, xcur=xoffset, ycur=yoffset;
             tableLayoutPanel1.ColumnCount = (int)(tableLayoutPanel1.Size.Width / buttonsize);
             tableLayoutPanel1.RowCount = (int)(tableLayoutPanel1.Size.Height / (buttonsize + 10 + 30));
             for (int i = 0; i < tableLayoutPanel1.ColumnCount; i++)
@@ -37,62 +37,82 @@ namespace scuffed_explorer
             {
                 tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100 / tableLayoutPanel1.RowCount));
             }
-            directoryButtons = new PictureBox[total];
+            directoryButtons = new Button[total];
             directoryLabels = new Label[total];
             for (int i = 0; i<childDirs.Length; i++)
             {
-                directoryButtons[i] = new PictureBox();
-                directoryButtons[i].Image = Image.FromFile("C:\\Users\\ghost\\source\\repos\\scuffed explorer\\scuffed explorer\\icons\\directory_scuffed.png");
+                directoryButtons[i] = new Button();
+                directoryButtons[i].BackgroundImage = Image.FromFile("C:\\Users\\ghost\\source\\repos\\scuffed-explorer\\scuffed explorer\\icons\\directory_scuffed.png");
                 directoryButtons[i].Tag = childDirs[i];
-                this.directoryButtons[i].Location = new System.Drawing.Point(0, 0);
+                directoryButtons[i].BackgroundImageLayout = ImageLayout.Stretch;
+                this.directoryButtons[i].Location = new System.Drawing.Point(xcur, ycur);
                 this.directoryButtons[i].Size = new System.Drawing.Size(buttonsize, buttonsize);
                 this.directoryButtons[i].Click += button_click;
-                this.tableLayoutPanel1.Controls.Add(directoryButtons[i], xoffset, yoffset);
+                this.directoryButtons[i].ContextMenuStrip = contextMenuStrip1;
+                this.groupBox1.Controls.Add(directoryButtons[i]);
 
                 directoryLabels[i] = new Label();
-                this.directoryLabels[i].Location = new System.Drawing.Point(0, buttonsize);
-                this.directoryLabels[i].Size = new System.Drawing.Size(buttonsize, yoffset - 10);
-                directoryLabels[i].Text = childDirs[i].Substring(path.Length+1);
-                this.tableLayoutPanel1.Controls.Add(directoryLabels[i], xoffset, yoffset);
+                this.directoryLabels[i].Location = new System.Drawing.Point(xcur, ycur+buttonsize);
+                this.directoryLabels[i].Size = new System.Drawing.Size(buttonsize, 30);
+                directoryLabels[i].Text = childDirs[i].Substring(path.Length);
+                this.groupBox1.Controls.Add(directoryLabels[i]);
 
 
-                xoffset++;
-                if (xoffset==tableLayoutPanel1.ColumnCount)
+                xcur+=xoffset+buttonsize;
+                if (xcur+buttonsize>=groupBox1.Size.Width)
                 {
-                    xoffset=1;
-                    yoffset++;
+                    xcur=xoffset;
+                    ycur+=yoffset+buttonsize;
                 }
             }
 
             int totalDirs = childDirs.Length;
             for (int i = 0; i < childFiles.Length; i++)
             {
-                directoryButtons[totalDirs + i] = new PictureBox();
-                directoryButtons[totalDirs + i].Image = Image.FromFile("C:\\Users\\ghost\\source\\repos\\scuffed explorer\\scuffed explorer\\icons\\file_scuffed.png");
-                this.directoryButtons[totalDirs + i].Location = new System.Drawing.Point(0, 0);
+                directoryButtons[totalDirs + i] = new Button();
+                directoryButtons[totalDirs + i].BackgroundImage = Image.FromFile("C:\\Users\\ghost\\source\\repos\\scuffed-explorer\\scuffed explorer\\icons\\file_scuffed.png");
+                directoryButtons[totalDirs + i].BackgroundImageLayout = ImageLayout.Stretch;
+                this.directoryButtons[totalDirs + i].Location = new System.Drawing.Point(xcur, ycur);
                 this.directoryButtons[totalDirs + i].Size = new System.Drawing.Size(buttonsize, buttonsize);
-                this.tableLayoutPanel1.Controls.Add(directoryButtons[totalDirs + i], xoffset, yoffset);
+                this.groupBox1.Controls.Add(directoryButtons[totalDirs + i]);
 
                 directoryLabels[totalDirs + i] = new Label();
-                this.directoryLabels[totalDirs + i].Location = new System.Drawing.Point(0, buttonsize);
+                this.directoryLabels[totalDirs + i].Location = new System.Drawing.Point(xcur, ycur+buttonsize);
                 this.directoryLabels[totalDirs + i].Size = new System.Drawing.Size(buttonsize, 30);
-                directoryLabels[totalDirs + i].Text = childFiles[i].Substring(path.Length+1);
-                this.tableLayoutPanel1.Controls.Add(directoryLabels[totalDirs + i], xoffset, yoffset);
+                directoryLabels[totalDirs + i].Text = childFiles[i].Substring(path.Length);
+                this.groupBox1.Controls.Add(directoryLabels[totalDirs + i]);
 
 
-                xoffset++;
-                if (xoffset == tableLayoutPanel1.ColumnCount)
+                xcur+=xoffset+buttonsize;
+                if (xcur + buttonsize >= groupBox1.Size.Width)
                 {
-                    xoffset=1;
-                    yoffset++;
+                    xcur=xoffset;
+                    ycur += yoffset+buttonsize;
                 }
             }
         }
 
         void button_click(object sender, EventArgs e)
         {
-            tableLayoutPanel1.Controls.Clear();
             drawDirectory(((sender as Button).Tag as String));
+        }
+
+        void deleteDir(object sender, EventArgs e)
+        {
+            ToolStripMenuItem mnu = sender as ToolStripMenuItem;
+            ContextMenuStrip mcm = (ContextMenuStrip)mnu.GetCurrentParent();
+            Button myButton = mcm.PlacementTarget as Button;
+            drawDirectory(textBox1.Text);   
+        }
+
+        void contextMenuDrop(object sender, EventArgs e)
+        {
+            MouseEventArgs me = (MouseEventArgs)e;
+            if (me.Button == System.Windows.Forms.MouseButtons.Right)
+            {
+                Button proj = (Button)sender;
+                proj.ContextMenuStrip = contextMenuStrip1;
+            }
         }
 
         private void GoButtonClicked(object sender, EventArgs e)
@@ -103,7 +123,11 @@ namespace scuffed_explorer
 
         private void moveUp(object sender, EventArgs e)
         {
-            string path = textBox1.Text;
+            if (textBox1.Text.LastIndexOf('\\')==textBox1.Text.Length-1)
+            {
+                return;
+            }
+            string path = textBox1.Text.Substring(0, textBox1.Text.LastIndexOf('\\')+1);
             drawDirectory(path);
         }
 
@@ -112,8 +136,10 @@ namespace scuffed_explorer
             string path = textBox1.Text;
             drawDirectory(path);
         }
+
         private void Form1_Load(object sender, System.EventArgs e)
         {
+            deleteToolStripMenuItem.Click += deleteDir;
             drawDirectory("C:\\");
         }
     }
